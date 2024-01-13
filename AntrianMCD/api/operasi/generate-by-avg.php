9005c7f00406a91194155cd215012f298e4864d5
ariyahandikam <50310586+ariyahandikam@users.noreply.github.com>
2024-01-13 15:43:26 +0700

    Update generate-by-avg.php

--- AntrianMCD/api/operasi/generate-by-avg.php
+++ AntrianMCD/api/operasi/generate-by-avg.php
@@ -10,22 +10,17 @@ include_once '../../models/Antrian.php';
 $database = new Database();
 $db = $database->getConnection();
 $item = new Antrian($db);
-$item->generateByAVG();
-
-if ($item->waktudatang != null) {
-    // Konversi nilai waktu ke format tertentu (misalnya, format H:i:s untuk jam:menit:detik)
-    $formattedwaktudatang = date('H:i:s', strtotime($item->waktudatang));
-    $formattedmaxwaktudatang = date('H:i:s', strtotime($item->max_waktudatang));
-
+$item->generateKesimpulan();
+if ($item->sk_avg != null) {
     // create response array
     $data_arr = array(
-        "waktudatang" => "Jika awal waktu kedatangan konsumen pada: " . $formattedwaktudatang,
-        "max_waktudatang" => " dan kedatangan pelanggan terakhir pada:" . $formattedmaxwaktudatang . ",",
-        "selisihkedatangan" => round($item->selisihkedatangan),
-        "selisihpelayanankasir" => round($item->selisihpelayanankasir),
-        "selisihkeluarantrian" => round($item->selisihkeluarantrian),
-        "selisihminkeluarantrian" => round($item->min_selisihkeluarantrian),
-        "selisihmaxkeluarantrian" => round($item->max_selisihkeluarantrian)
+        "kesimpulan_waktu_tunggu" => "Pada entitas antrian yaitu entitas waktu tunggu
+        pada 1 kasir MCD Solo Grand Mall memiliki waktu tunggu minimal sebesar " . round($item->sk_min) . " Menit dengan waktu tunggu maksimal sebesar " .
+        round($item->sk_max) . " Menit dengan rata-rata waktu tunggu sebesar " .
+        date('i:s', $item->sk_avg). " Menit",
+        "kesimpulan_banyak_antrian" => "Pada entitas antrian yaitu entitas banyak antrian
+        pada 1 kasir MCD Solo Grand Mall memiliki banyak antrian minimal sebesar " .
+        round($item->spk_max)." Konsumen dengan rata_rata waktu tunggu sebesar ".$item->spk_avg." Konsumen dan jika dibulatkan menjadi ". round($item->spk_avg)." konsumen", 
     );
 
     http_response_code(200);
